cedcc2f991455b194554bef8ffe40cf05b5b702d
MelodyC823 <69552287+MelodyC823@users.noreply.github.com>
2022-09-10 16:16:48 -0700

    Add files via upload

---
+++ 2110/2110/DataAccess/IReadConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2110.DataAccess
+{
+    public interface IReadConfiguration
+    {
+        string GetConnectionString(string configName);
+    }
+}
---
+++ 2110/2110/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using _2110.DataAccess;
+using System;
+using Microsoft.Data.SqlClient;
+
+namespace _2110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateTable();
+        }
+
+        public static void CreateTable()
+        {
+            var readConfig = new ReadConfiguration();
+            var connString = readConfig.GetConnectionString(EnvironmentSettings.DBEnvironment);
+
+            Console.WriteLine(connString);
+
+            var sqlConnection = new SqlConnection(connString);
+
+            string sqlStatement = "CREATE TABLE Employee(ID VARCHAR(30) NOT NULL, NAME VARCHAR(50) NOT NULL)";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+    }
+}
