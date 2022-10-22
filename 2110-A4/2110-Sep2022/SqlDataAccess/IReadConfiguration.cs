5c2c7f382363daf78bca6b01c921be267b1e57b4
Yifei Chen <000442683@studentvcc.onmicrosoft.com>
2022-10-21 17:11:45 -0700

    Add A4

---
+++ 2110-A4/2110-Sep2022/SqlDataAccess/IReadConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sept2022.SqlDataAccess
+{
+    public interface IReadConfiguration
+    {
+        string GetConnectionString(string configName);
+    }
+}
